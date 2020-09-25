241b0ffc61d556e1b09c32bf6ca20c41eb12434d
nolPV <nolnetlog@gmail.com>
2020-09-25 15:52:30 +0700

    Create Navbar

---
+++ pod-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { Menu, Segment } from 'semantic-ui-react'
+
+class Navbar extends Component {
+    state = { activeItem: 'home' }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <div>
+                <Menu pointing secondary>
+
+                    <Menu.Menu position='right'>
+                        <Menu.Item
+                            name='CREATE'
+                            active={activeItem === 'create'}
+                            onClick={this.handleItemClick}
+                        />
+                        <Menu.Item
+                            name='ARTIST'
+                            active={activeItem === 'artist'}
+                            onClick={this.handleItemClick}
+                        />
+                        <Menu.Item
+                            name='ALL PRODUCT'
+                            active={activeItem === 'product'}
+                            onClick={this.handleItemClick}
+                        />
+                        <Menu.Item
+                            name='SHOP'
+                            active={activeItem === 'shop'}
+                            onClick={this.handleItemClick}
+                        />
+                    </Menu.Menu>
+                </Menu>
+
+
+            </div>
+        )
+    }
+}
+
+export default Navbar
